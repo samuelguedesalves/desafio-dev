63d7fc44381b55df48f814fdfe39780ef42ba678
Samuel Guedes <guedes.works7@gmail.com>
2022-06-02 18:23:43 -0400

    feat: create function to get a user by email from database

---
+++ backend/lib/backend/user/get.ex
@@ -0,0 +1,15 @@
+defmodule Backend.User.Get do
+  alias Backend.Repo
+  alias Backend.User
+
+  import Ecto.Query
+
+  def by_email(email) do
+    query = from u in User, where: u.email == ^email
+
+    case Repo.one(query) do
+      nil -> {:error, :user_not_found}
+      user -> {:ok, user}
+    end
+  end
+end
