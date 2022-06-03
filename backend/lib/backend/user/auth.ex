66c4e0513118b9aba2edac90d074c1b819b6bd6f
Samuel Guedes <guedes.works7@gmail.com>
2022-06-03 01:28:34 -0400

    refactor: change alias into auth module

--- backend/lib/backend/user/auth.ex
+++ backend/lib/backend/user/auth.ex
@@ -1,17 +1,18 @@
 defmodule Backend.User.Auth do
-  alias Backend.{Guardian, User}
+  alias Backend.Guardian, as: GuardianAuth
+  alias Backend.User
   alias User.Get, as: GetUser
 
   def call(%{email: email, password: password}) do
     with {:ok, %User{id: id, password_hash: password_hash} = user} <- GetUser.by_email(email),
          :ok <- check_password(password, password_hash),
-         {:ok, access_token, _} <- Guardian.encode_and_sign(id, %{}, ttl: {24, :hours}) do
+         {:ok, access_token, _} <- GuardianAuth.encode_and_sign(id, %{}, ttl: {24, :hours}) do
       {:ok, user, access_token}
     end
   end
 
   def call(%User{id: id} = user) do
-    {:ok, access_token, _} = Guardian.encode_and_sign(id, %{}, ttl: {24, :hours})
+    {:ok, access_token, _} = GuardianAuth.encode_and_sign(id, %{}, ttl: {24, :hours})
 
     {:ok, user, access_token}
   end
