29a6123e81a4b49ef557b1955d11199188233a0b
Samuel Guedes <guedes.works7@gmail.com>
2022-06-03 16:57:11 -0400

    chore: make user creation unitary test

---
+++ backend/test/user/user_test.exs
@@ -0,0 +1,45 @@
+defmodule Backend.UserTest do
+  use ExUnit.Case, assync: true
+
+  alias Backend.{Repo, User}
+  alias Backend.User.Create, as: CreateUser
+
+  import Backend.Factory
+
+  describe "call/1" do
+    setup do
+      :ok = Ecto.Adapters.SQL.Sandbox.checkout(Repo)
+    end
+
+    test "when all map fields are valid, create a user" do
+      user_attrs = build(:user_attrs)
+
+      response = CreateUser.call(user_attrs)
+
+      assert {:ok, %User{} = user} = response
+      assert user_attrs.name == user.name
+      assert user_attrs.email == user.email
+      assert user.password_hash != nil
+    end
+
+    test "when map fields are invalid, return a changeset with erros" do
+      user_attrs = build(:user_attrs, email: "samuel#gmail.com", password: "1234")
+
+      response = CreateUser.call(user_attrs)
+
+      expected_errors = [
+        password: {
+          "should be at least %{count} character(s)",
+          [count: 6, validation: :length, kind: :min, type: :string]
+        },
+        email: {
+          "has invalid format",
+          [validation: :format]
+        }
+      ]
+
+      assert {:error, %Ecto.Changeset{valid?: false} = changeset} = response
+      assert expected_errors == changeset.errors
+    end
+  end
+end
