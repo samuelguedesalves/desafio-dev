3046251fc10e1c5e5d8390e295a7534ad0debbef
Samuel Guedes <guedes.works7@gmail.com>
2022-06-08 15:21:14 -0400

    chore: create global styles

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { GlobalStyles } from './styles/global';
 
 function App() {
   return (
-    <div className="App">
-      <h1>hello world</h1>
-    </div>
+    <>
+      <div className="App">
+        <h1>hello world</h1>
+      </div>
+      <GlobalStyles/>
+    </>
   );
 }
 
---
+++ frontend/src/styles/global.ts
@@ -0,0 +1,8 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyles = createGlobalStyle`
+* {
+    box-sizing: border-box;
+    font-family: 'Roboto', sans-serif;
+}
+`;
