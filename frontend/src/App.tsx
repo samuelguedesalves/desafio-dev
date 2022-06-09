cbc0c7c6ff1e12993147db6a81da80020cf9c1f8
samuel guedes <guedes.works7@gmail.com>
2022-06-09 18:37:36 -0400

    feat: set router and auth provider

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import { Dashboard } from './components/Dashboard';
+import { LoginPage } from './components/LoginPage';
+import { AuthProvider } from './hooks/useAuth';
+
 import { GlobalStyles } from './styles/global';
 
 function App() {
   return (
     <>
-      <div className="App">
-        <h1>hello world</h1>
-      </div>
+      <AuthProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<LoginPage/>} />
+            <Route path="/dashboard" element={<Dashboard/>} />
+          </Routes>
+        </BrowserRouter>
+      </AuthProvider>
       <GlobalStyles/>
     </>
   );
