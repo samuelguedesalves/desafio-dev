2cf74c5806d41d171f72870b6b329e8367d3ab30
samuel guedes <guedes.works7@gmail.com>
2022-06-09 18:36:23 -0400

    feat: create login page component

--- frontend/src/styles/global.ts
+++ frontend/src/styles/global.ts
@@ -1,8 +1,23 @@
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyles = createGlobalStyle`
+	:root {
+		--light: #eeeeee;
+		--dark: #222222;
+		--primary: #861388;
+		--primary-dark: #621064;
+	}
+
 * {
+		margin: 0;
+		padding: 0;
     box-sizing: border-box;
     font-family: 'Roboto', sans-serif;
 }
+
+	html, body {
+		margin: 0;
+		padding: 0;
+		background: var(--light);
+	}
 `;
