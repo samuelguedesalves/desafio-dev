0ff3e25d68a559c5c01ea1a41d7feead2b10e9db
samuel guedes <guedes.works7@gmail.com>
2022-06-14 15:13:44 -0400

    chore: create types

---
+++ frontend/src/types/Transaction.ts
@@ -0,0 +1,13 @@
+export type Transaction = {
+  amount: number;
+  card: string;
+  cpf: string;
+  date: string;
+  hour: string;
+  id: number;
+  inserted_at: string;
+  shop_name: string;
+  shop_owner: string;
+  type: string;
+  updated_at: string;
+};
---
+++ frontend/src/types/User.ts
@@ -0,0 +1,7 @@
+export type User = {
+  id: number;
+  email: string;
+  inserted_at: string;
+  name: string;
+  updated_at: string;
+};
