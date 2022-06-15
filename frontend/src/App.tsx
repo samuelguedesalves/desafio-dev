779155611b484cab36d983009e2968d060a49506
samuel guedes <guedes.works7@gmail.com>
2022-06-15 17:09:22 -0400

    fix: adjustment at router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -14,7 +14,7 @@ const App: React.FC = () => (
       <ShopsProvide>
         <BrowserRouter>
           <Routes>
-            <Route path="/" element={<LoginPage />} />
+            <Route path="/*" element={<LoginPage />} />
             <Route path="/dashboard/*" element={<Dashboard />} />
           </Routes>
         </BrowserRouter>
